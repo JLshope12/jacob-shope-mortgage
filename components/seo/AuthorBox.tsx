700dc59dc0cd0e75ccbee572a3c6ba53be65acd9
JLshope12 <shope@mpirefi.com>
2026-09-08 13:35:50 -0400

    Add Google Business Profile corroboration to author box

--- components/seo/AuthorBox.tsx
+++ components/seo/AuthorBox.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 
 const NMLS_URL = "https://www.nmlsconsumeraccess.org/EntityDetails.aspx/INDIVIDUAL/2090979";
 const MPIRE_URL = "https://www.mpirefinancialgroup.com/";
+const GOOGLE_BUSINESS_URL =
+  "https://www.google.com/maps/search/?api=1&query=Jacob+Shope+Mortgage+Broker&query_place_id=ChIJoYVzko0AvIcRA-l3WlYdLW8";
 
 const LOCAL_AREAS = [
   ["Charlotte", "/service-areas/charlotte"],
@@ -32,6 +34,7 @@ export function AuthorBox() {
         <Link href="/charlotte-home-buying-mortgage-guide" className="text-gold hover:underline">Charlotte Home Buying Guide</Link>
         <a href={NMLS_URL} target="_blank" rel="noopener noreferrer" className="text-gold hover:underline">Verify NMLS# 2090979</a>
         <a href={MPIRE_URL} target="_blank" rel="noopener noreferrer" className="text-gold hover:underline">Mpire Financial</a>
+        <a href={GOOGLE_BUSINESS_URL} target="_blank" rel="noopener noreferrer" className="text-gold hover:underline">Google Business &amp; Reviews</a>
         <a href="https://www.instagram.com/jlshopeloans/" target="_blank" rel="noopener noreferrer me" className="text-gold hover:underline">Instagram</a>
         <a href="https://www.facebook.com/jacob.shope.33" target="_blank" rel="noopener noreferrer me" className="text-gold hover:underline">Facebook</a>
       </div>
