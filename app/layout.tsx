2136f7c63ae00092f431a6b211ca5c140d27625d
JLshope12 <shope@mpirefi.com>
2026-09-11 09:10:07 -0400

    Add Google Business profile to Jacob sameAs entity signals

--- app/layout.tsx
+++ app/layout.tsx
@@ -128,6 +128,7 @@ export default function RootLayout({
                 ],
                 sameAs: [
                   NMLS_URL,
+                  GOOGLE_BUSINESS_URL,
                   "https://www.linkedin.com/in/jacob-shope-9b8972282",
                   "https://www.instagram.com/jlshopeloans/",
                   "https://www.facebook.com/jacob.shope.33",
