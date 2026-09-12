b60bda09f93e369eb51d7a81f39d3ad9c04585b2
JLshope12 <shope@mpirefi.com>
2026-09-12 08:01:37 -0400

    Add current LinkedIn corroboration to author boxes

--- components/seo/AuthorBox.tsx
+++ components/seo/AuthorBox.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 const NMLS_URL = "https://www.nmlsconsumeraccess.org/EntityDetails.aspx/INDIVIDUAL/2090979";
 const MPIRE_URL = "https://www.mpirefinancialgroup.com/";
+const LINKEDIN_URL = "https://www.linkedin.com/in/jacob-shope-9b8972282";
 const GOOGLE_BUSINESS_URL =
   "https://www.google.com/maps/search/?api=1&query=Jacob+Shope+Mortgage+Broker&query_place_id=ChIJoYVzko0AvIcRA-l3WlYdLW8";
 
@@ -35,6 +36,7 @@ export function AuthorBox() {
         <a href={NMLS_URL} target="_blank" rel="noopener noreferrer" className="text-gold hover:underline">Verify NMLS# 2090979</a>
         <a href={MPIRE_URL} target="_blank" rel="noopener noreferrer" className="text-gold hover:underline">Mpire Financial</a>
         <a href={GOOGLE_BUSINESS_URL} target="_blank" rel="noopener noreferrer" className="text-gold hover:underline">Google Business &amp; Reviews</a>
+        <a href={LINKEDIN_URL} target="_blank" rel="noopener noreferrer me" className="text-gold hover:underline">LinkedIn</a>
         <a href="https://www.instagram.com/jlshopeloans/" target="_blank" rel="noopener noreferrer me" className="text-gold hover:underline">Instagram</a>
         <a href="https://www.facebook.com/jacob.shope.33" target="_blank" rel="noopener noreferrer me" className="text-gold hover:underline">Facebook</a>
       </div>
