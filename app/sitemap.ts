ddc47b3bb5ecbf5a74a2ebc0dbcef0272e1b24fd
JLshope12 <shope@mpirefi.com>
2026-09-19 08:59:52 -0400

    SEO: align sitemap last-modified dates with actual updates

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,8 +4,8 @@ import { LOAN_PROGRAMS } from "@/data/loan-programs";
 
 const origin = "https://jacobshopemortgage.com";
 const siteRefreshDate = new Date("2026-09-15T00:00:00-04:00");
-const aboutRefreshDate = new Date("2026-09-17T00:00:00-04:00");
-const firstTimeBuyerRefreshDate = new Date("2026-09-17T00:00:00-04:00");
+const aboutRefreshDate = new Date("2026-09-19T00:00:00-04:00");
+const firstTimeBuyerRefreshDate = new Date("2026-09-18T00:00:00-04:00");
 const consolidatedProgramSlugs = new Set(["first-time-buyer", "va"]);
 
 export default function sitemap(): MetadataRoute.Sitemap {
